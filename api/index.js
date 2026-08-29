1009a4035e1c25f35a6371cba5e1d3c0f14dfb4a
Rahul-Ramanath-Shanbhag17 <PES1UG23AM226@pesu.pes.edu>
2026-08-29 15:02:12 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+} )  
 
 const app=express();
 
